23b0269d5464d176a85c1c0f951e387add256b93
MinGyu, Ju <knighp@gmail.com>
2020-12-23 12:51:58 +0900

    Update HTTPPOSTTrigger.cs

--- HTTPPOSTTrigger.cs
+++ HTTPPOSTTrigger.cs
@@ -28,38 +28,7 @@ namespace RecursiveSoft.Function
         {
             string dateA = DateTime.UtcNow.AddHours(9).ToString("yyyyMM");
             string dateB = DateTime.UtcNow.AddHours(9).ToString("yyyyMMdd");
-
-            string connStrA = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connStrA);
-            BlobContainerClient containerA = blobServiceClient.GetBlobContainerClient("jsondata");
-            BlobClient blobClientA = containerA.GetBlobClient(dateA + "/" + dateB + ".json");
-            //AppendBlobClient blobClientA = containerA.GetAppendBlobClient(dateA + "/" + dateB + ".json");
-            //appendBlob.AppendText("new line");
             
-            JArray dailyArr = new JArray();
-            string jsonA = new StreamReader(req.Body).ReadToEnd();
-            JObject jsonObj = JObject.Parse(jsonA);
-            
-            if (await blobClientA.ExistsAsync())
-            {
-                using (MemoryStream msA = new MemoryStream())
-                {
-                    await blobClientA.DownloadToAsync(msA);
-                    var length = msA.Length;
-                    if (length != 0)
-                    {
-                        string dailyJson = System.Text.Encoding.UTF8.GetString(msA.ToArray());
-                        dailyArr = JArray.Parse(dailyJson);
-                    }
-                }
-            }
-            dailyArr.Add(jsonObj);
-            
-            string dayJson = Newtonsoft.Json.JsonConvert.SerializeObject(dailyArr);
-            using (Stream streamA = new MemoryStream(Encoding.UTF8.GetBytes(dayJson)))
-            {
-                await blobClientA.UploadAsync(streamA, true);
-            }
         }
     }
 }
